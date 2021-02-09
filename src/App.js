5a76730eee4746c85be1e8727804348fb8a7ff3b
hyf <rufaidamohammadjumaa@gmail.com>
2021-02-09 01:47:35 +0100

    Add handleNumberClick to test the clicked number a update status

--- src/App.js
+++ src/App.js
@@ -5,23 +5,54 @@ import Star from './components/Star';
 import PlayNumber from './components/PlayNumber';
 
 const StarMatch = () => {
-/*   const [playNum,setplayNum] = useState(9);
- */  const [stars,setStars] = useState(9);
-     const [availableNum,setAvailableNum]= useState([8,1,6,2,3,5,7]);
-     const [candidateNum,setCandidateNum]= useState([3,5,]);
-     
-     const buttonStatus= (buttonId) => {
-      if (!availableNum.includes(buttonId)){
-        return 'used'
+  const [stars, setStars] = useState(9);
+  const [availableNum, setAvailableNum] = useState(utils.range(1, 9));
+  const [candidateNum, setCandidateNum] = useState([]);
+
+  const buttonStatus = (buttonId) => {
+    if (!availableNum.includes(buttonId)) {
+      return 'used'
+    }
+    if (candidateNum.includes(buttonId)) {
+      if (utils.sum(candidateNum) > stars) {
+        return 'wrong'
       }
-      if (candidateNum.includes(buttonId)){
-        if (utils.sum(candidateNum) > stars){
-           return 'wrong'
-        }
       return 'candidate'
+    }
+    return 'available'
+  }
+
+  const handleNumberClick = (id, status) => {
+    console.log('handleClick', id);
+    console.log('CandidateNum', candidateNum);
+    console.log('availableNium', availableNum);
+    /* If it is not available */
+    if (status !== 'yellow') {
+      console.log('not available', status)
+      if (status === 'blue') {
+        console.log('candidate', status)
+        return;
       }
-      return 'available'
-     }
+      if (status === 'red') {
+        console.log('wrong', status);
+        setCandidateNum(candidateNum.filter((elem) => elem !== id))
+        return;
+      }
+      return;
+    }
+    /* If it is available */
+      const newCandidatNum = candidateNum.concat(id);
+      console.log('newcandidate', newCandidatNum);
+      if (utils.sum(newCandidatNum) > stars) {
+        setCandidateNum(newCandidatNum)
+      } else if (utils.sum(newCandidatNum) < stars) {
+        setCandidateNum(newCandidatNum)
+      } else {
+        setAvailableNum(availableNum.filter((elem) => !newCandidatNum.includes(elem)));
+        setCandidateNum([]);
+      }
+    
+  }
 
   return (
     <div className="game">
@@ -30,23 +61,28 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          {utils.range(1,stars).map(starId=> <Star key={starId}/>)}
+          {utils.range(1, stars).map(starId => <Star key={starId} />)}
         </div>
         <div className="right">
-          {utils.range(1,9).map(buttonId=> <PlayNumber key={buttonId} buttonId={buttonId} status={statusColors[buttonStatus(buttonId)]} />)}
+          {utils.range(1, 9).map(buttonNumber =>
+            <PlayNumber key={buttonNumber}
+              buttonId={buttonNumber}
+              status={statusColors[buttonStatus(buttonNumber)]}
+              onClick={handleNumberClick}
+            />)}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
     </div>
   );
-  
+
 };
 // Status colors
-const statusColors ={
-  'used' : 'green',
+const statusColors = {
+  'used': 'green',
   'wrong': 'red',
   'candidate': 'blue',
-  'available': 'grey'
+  'available': 'yellow'
 }
 // Math science
 const utils = {
